9b2b0dac17bdf37ab87b3d184df831706c990b59
dl <dilshanka@konekt.lk>
2019-12-23 22:47:49 +0530

    index changes

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="components/bootstrap/bootstrap.css">
+        <link rel="stylesheet" href="components/bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="components/jquery-confirm/jquery-confirm.css">
+        <link rel="stylesheet" href="components/jquery-confirm/jquery-confirm.min.css">
+        <link rel="stylesheet" href="components/data-tables/datatables.css">
+        <link rel="stylesheet" href="components/data-tables/datatables.min.css">
+    <title>SMG</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark">
+        <div class="container-fluid">
+            <div class="navbar-header" align="center">
+                <a class="navbar-brand" href="#"><b>Simple Student Management CRUD</b></a>
+            </div>
+        </div>
+    </nav>
+
+</body>
+</html>
